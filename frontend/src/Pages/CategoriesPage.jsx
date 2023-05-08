3ac0003e69de2d5531996cb5be26cbad7ce47719
mtdevery <mattdevery@gmail.com>
2023-05-07 20:26:30 -0500

    Initial commit with react boilerplate, side nav, and header using react-pro-sidebar and material-ui

---
+++ frontend/src/Pages/CategoriesPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CategoriesPage(){
+    return(
+        <>
+            <h2>Categories</h2>
+        </>
+    );
+}
+
+export default CategoriesPage;
---
+++ frontend/src/Pages/UserEventsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UserEventsPage(){
+    return(
+        <>
+            <h2>User Events</h2>
+        </>
+    );
+}
+
+export default UserEventsPage;
