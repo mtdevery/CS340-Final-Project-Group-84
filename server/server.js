46c110dd49d753d681c4de838a077dcad613005a
mtdevery <mattdevery@gmail.com>
2023-05-20 12:07:08 -0500

    Fixed bug with routing and added events controller. Added dropdown to events page to filter by location.

--- server/server.js
+++ server/server.js
@@ -4,19 +4,22 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Serve static react app
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.get('/api/events', (req, res) => {
-    res.json([
-        { EventId: 1, Time: '08/18/2023 1:00 AM', Description: 'Bridal Party for Abbie', Cost: 17.93, LocationId: 1},
-        { EventId: 2, Time: '06/22/2023 4:58 PM', Description: 'Rolling Stones Concert', Cost: 37.97, LocationId: 2},
-        { EventId: 3, Time: '06/07/2023 4:17 PM', Description: 'Sunset Film Festival', Cost: 23.84, LocationId: 3},
-        { EventId: 4, Time: '02/06/2023 6:06 AM', Description: 'Live Band Karaoke', Cost: 39.73, LocationId: 2},
-        { EventId: 5, Time: '11/08/2023 8:00 AM', Description: 'Abbie\'s Wedding', Cost: 2.60, LocationId: 1}
-    ]);
+/*
+    Add controllers
+*/
+app.use('/Controllers/events-controller')
+
+// Send routing back to react app
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
-// Start the server
+/* 
+    Start the server
+*/
 const PORT = process.env.PORT || 14443;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
