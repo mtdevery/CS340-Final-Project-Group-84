004656de7a3f9aaabfac874323fac454cdabe22e
oluyoler <oluyoler@oregonstate.edu>
2023-05-24 22:22:33 -0400

    basic CRUD, added db connector GET,new middleware package.json

---
+++ server/db-connector.js
@@ -0,0 +1,14 @@
+var mysql = require('mysql')
+
+// 
+var pool = mysql.createPool({
+    connectionLimit : 10,
+    host            : 'classmysql.engr.oregonstate.edu',
+    user            : 'cs340_oluyoler',
+    password        : '0906',
+    database        : 'cs340_oluyoler'
+})
+
+// Export it for use in our application
+module.exports.pool = pool;
+
--- server/server.js
+++ server/server.js
@@ -1,27 +1,73 @@
 'use strict';
-
-const express = require('express');
-const path = require('path');
-const app = express();
-
 // Serve static react app
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+//app.use(express.static(path.join(__dirname, '../frontend/build')));
+'use strict' ;
+const express = require('express');   
+const app     = express();           
+const PORT = process.env.PORT || 62490;                 
+const db = require('./db-connector')
+const bodyParser = require('body-parser') ;
 
-/*
-    Add controllers
-*/
-app.use('/Controllers/events-controller')
+// Middleware 
+app.use(express.json()) ;
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }))
 
-// Send routing back to react app
-app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+//test get connection 
+/*app.get('/',(req,res) =>{
+    res.send("Express Servers is up")
+});*/
+
+app.get('/display-events', (req, res) => {
+        let query1 = 'SELECT * from Events;'
+        db.pool.query(query1, function(err, results, fields){
+        res.send( JSON.stringify(results));
+    });   
 });
 
-/* 
-    Start the server
-*/
-const PORT = process.env.PORT || 14443;
-app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-    console.log('Press Ctrl+C to quit.');
+// create new event 
+app.post('/add-events',(req,res)=>{
+    let event = req.body;
+    let new_query = `INSERT INTO Events(Time, Description, Cost, LocationId) VALUES (${event.Time},${event.Description},${event.Cost},${event.LocationId});`
+    db.pool.query(new_query, function(err, results, fields){
+    });
 });
+
+
+app.delete('/delete-events',(req,res)=>{
+    let event = req.body;
+    let query = `DELETE FROM Events WHERE EventId = ${event.EventId}; `;
+    db.pool.query(query,(err,results,fields) =>{
+    });
+});
+
+
+app.put('/update-event',(req,res)=>{
+    let event = req.body;
+    const id = event.EventId;
+    const Time = event.Time;
+    const Description = event.Description;
+    const Cost = event.Cost;
+    const LocationId = event.LocationId;
+
+
+    let query = `UPDATE Events SET Time=${Time},Description=${Description},Cost=${Cost},LocationId=${LocationId} WHERE EventId=${id};`
+    db.pool.query(query,(err,results,fields) =>{
+
+    });
+});
+
+app.delete('/delete-event',(req,res,)=>{
+    let event = req.body;
+    let id = event.EventId;
+    let query = `DELETE FROM Events WHERE Event.EventId = ${id};`
+
+    db.pool.query(query,(err,results,fields) =>{
+
+    });
+});
+
+app.listen(PORT, function(){           
+    console.log(`Express Server listening on port ${PORT}`)
+});
+
